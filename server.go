2fe70112f5c2fb0fef2f00061584b1ac56a80948
3dw1nM0535 <workockmoses@gmail.com>
2021-03-10 17:02:57 +0300

    feat:recovery function

--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/3dw1nM0535/galva/graph"
 	"github.com/3dw1nM0535/galva/graph/generated"
@@ -23,19 +26,17 @@ func init() {
 
 func main() {
 
-	orm, err := store.Factory()
+	orm, err := store.NewORM()
 	if err != nil {
 		fmt.Printf("Error while setting up ORM: " + err.Error())
 	}
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.New(orm)}))
-	/*
-	 *srv.SetRecoverFunc(func(ctx context.Context, err interface{}) (userMessage error) {
-	 *        // send panic to sentry
-	 *        log.Print(err)
-	 *        debug.PrintStack()
-	 *        return errors.New("user message on panic")
-	 *})
-	 */
+	srv.SetRecoverFunc(func(ctx context.Context, err interface{}) (userMessage error) {
+		// send panic to sentry
+		log.Print(err)
+		debug.PrintStack()
+		return errors.New("user message on panic")
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
