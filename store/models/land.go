a83e73a042df228ce6e3cad81621e4773a8f3a76
3dw1nM0535 <workockmoses@gmail.com>
2021-03-10 08:53:41 +0300

    user index and localation indexed queries

--- store/models/land.go
+++ store/models/land.go
@@ -2,15 +2,18 @@ package models
 
 import (
 	"math/big"
+	"time"
 )
 
 // Land represent land asset
 type Land struct {
-	Model
-	TokenId    int      `gorm:"type:integer;"`
-	Title      string   `gorm:"type:varchar(50);"`
-	Size       *big.Int `gorm:"type:integer;"`
-	SizeUnit   string   `gorm:"type:varchar(10);"`
-	PostalCode int      `gorm:"type:integer;"`
-	Location   string   `gorm:"type:varchar(100);"`
+	ID         *ID       `gorm:"primary_key;not_null;"`
+	TokenId    int       `gorm:"type:integer;"`
+	Title      string    `gorm:"type:varchar(50);"`
+	Size       *big.Int  `gorm:"type:bigint;"`
+	SizeUnit   string    `gorm:"type:varchar(10);"`
+	PostalCode int       `gorm:"type:integer;"`
+	Location   string    `gorm:"type:varchar(100);"`
+	CreatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP;"`
+	UpdatedAt  time.Time
 }
