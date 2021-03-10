8fdbf4823fede3d24a200931074d20919cc7cce3
3dw1nM0535 <workockmoses@gmail.com>
2021-03-10 06:17:14 +0300

    chore:setup ORM

--- server.go
+++ server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/3dw1nM0535/galva/graph"
 	"github.com/3dw1nM0535/galva/graph/generated"
+	"github.com/3dw1nM0535/galva/store"
 	"github.com/3dw1nM0535/galva/utils"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -21,7 +26,17 @@ func init() {
 
 func main() {
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.New()}))
+	orm, err := store.Factory()
+	if err != nil {
+		fmt.Printf("Error while setting up ORM: " + err.Error())
+	}
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.New(orm)}))
+	srv.SetRecoverFunc(func(ctx context.Context, err interface{}) (userMessage error) {
+		// send panic to sentry
+		log.Print(err)
+		debug.PrintStack()
+		return errors.New("user message on panic")
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
