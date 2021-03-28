8ff0e94067a3e7f4b30f2fd54410f096e994e1b1
3dw1nM0535 <workockmoses@gmail.com>
2021-03-28 16:17:37 +0300

    chore(refactor): nft contract address

--- constants/constants.go
+++ constants/constants.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	ForbiddenToOwner    = "forbidden only to offer author"
-	NftContractAddress  = "0x4a41676f4BF4EEd9b07005CE93e0991d1E0E471e"
+	NftContractAddress  = "0xB933136f65E8034DD22618Dd2E61C9412D7992Cd"
 	NonExistentProperty = "cannot find property"
 	NonExistentUser     = "cannot find user"
 	Expired             = "offer already expired"
