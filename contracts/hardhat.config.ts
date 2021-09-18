7221b4ae2da4cf8efb5bb2d0c44e1cfc505f3f83
3dw1nM0535 <workockmoses@gmail.com>
2021-09-18 06:16:49 +0300

    chore(contract-setup): setup contract dev

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,11 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.7.3",
+};
+
+export default config;
