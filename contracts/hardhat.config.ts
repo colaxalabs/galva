310811c7d0d9b29925421637aa4d5959d8af4431
3dw1nM0535 <workockmoses@gmail.com>
2021-09-18 08:51:20 +0300

    chore(setup): more setup

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,7 +5,21 @@ import "@nomiclabs/hardhat-waffle";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.3",
+  solidity: {
+    version: "0.7.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./src/contracts",
+  },
+  mocha: {
+    timeout: 20000,
+  },
 };
 
 export default config;
