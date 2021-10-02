36b65b02569f506144ad8a85257c9192741d67eb
3dw1nM0535 <workockmoses@gmail.com>
2021-10-02 12:54:40 +0300

    chore(deploy): deployments

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,4 +1,6 @@
 import { task } from "hardhat/config";
+import { Contract } from "ethers";
+import fs from "fs";
 
 task("galva:deploy", "Deploy Galva Contracts").setAction(async (args, hre) => {
   // get signer
@@ -9,4 +11,36 @@ task("galva:deploy", "Deploy Galva Contracts").setAction(async (args, hre) => {
     .connect(deployer)
     .deploy();
   console.log("Galva deployed at", registry.address);
+  // save contract client-side
+  saveAbiClientSide(registry);
 });
+
+function saveAbiClientSide(registry: Contract) {
+  const dir = "/home/lomolo/Projects/galva/client/contracts";
+  // check if path exists
+  if (!fs.existsSync(dir)) {
+    // create path
+    fs.mkdirSync(dir);
+  }
+
+  // write contract deployed addresses
+  fs.writeFileSync(
+    dir + "/contracts.json",
+    JSON.stringify(
+      {
+        Registry: registry.address,
+      },
+      undefined,
+      2
+    )
+  );
+
+  // write contracts artifacts
+  const registryArtifacts = require(__dirname +
+    "/../artifacts/contracts/GalvaRegistry.sol/GalvaRegistry.json");
+  fs.writeFileSync(
+    dir + "/Registry.json",
+    JSON.stringify(registryArtifacts, null, 1)
+  );
+  console.log("Registry artifacts saved!");
+}
