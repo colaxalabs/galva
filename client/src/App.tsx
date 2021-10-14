1594e067550dea61ded529675068710e41b72ba6
3dw1nM0535 <workockmoses@gmail.com>
2021-10-14 23:35:18 +0300

    chore(setup): detect ethereum browser

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // do we have an ethereum browser
+  const [isEthereumWindow, setIsEthereumWindow] = useState<boolean>(false);
+
+  useEffect(() => {
+    // @ts-ignore
+    setIsEthereumWindow(window.ethereum !== undefined);
+  }, [isEthereumWindow])
+
   return (
     <div>
       Galva
